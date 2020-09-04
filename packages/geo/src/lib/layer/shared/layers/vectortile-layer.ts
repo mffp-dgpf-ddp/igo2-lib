7dc7ec89c58c343ad6db01997641775bdcc34690
PE Lord <pe_lord@pm.me>
2020-09-04 15:08:25 -0400

    wip

--- packages/geo/src/lib/layer/shared/layers/vectortile-layer.ts
+++ packages/geo/src/lib/layer/shared/layers/vectortile-layer.ts
@@ -38,60 +38,64 @@ export class VectorTileLayer extends Layer {
     //   });
     // }
 
-    // if (this.mtqInterceptor) {
-    //   vectorTileSource.setTileLoadFunction((tile, url) => {
-    //     const loader = xhr(url, tile.getFormat(), tile.onLoad.bind(tile), tile.onError.bind(tile));
-    //     tile.setLoader(loader);
-    //   });
-    // }
-
     if (this.mtqInterceptor) {
       vectorTileSource.setTileLoadFunction((tile, url) => {
-
-        // tile.setLoader(this.customLoader(tile, url, this.mtqInterceptor, vectorTileSource))
-        const loader = this.loadFeaturesXhr(url, tile.getFormat(), tile.onLoad.bind(tile));
+        const loader = this.customLoader(url, tile.getFormat(), this.mtqInterceptor, tile.onLoad.bind(tile));
         tile.setLoader(loader);
       }
       );
-
     }
     return vectorTile;
   }
 
-  loadFeaturesXhr(url, format, success) {
+  customLoader(url, format, interceptor, success, failure?) {
     return ((extent, resolution, projection) => {
       const xhr = new XMLHttpRequest();
       xhr.open('GET', typeof url === 'function' ? url(extent, resolution, projection) : url, true);
-      console.log(format, format.getType());
-      // if (format.getType() === FormatType.ARRAY_BUFFER) {
-      //   xhr.responseType = 'arraybuffer';
-      // }
+      const intercepted = interceptor.interceptXhr(xhr);
+      if (!intercepted) {
+        xhr.abort();
+        return;
+      }
+      if (format.getType() === 'arraybuffer') {
+        xhr.responseType = 'arraybuffer';
+      }
       xhr.onload = (event) => {
         if (!xhr.status || xhr.status >= 200 && xhr.status < 300) {
           const type = format.getType();
-          let source = void 0;
-          /*if (type == FormatType.JSON || type == FormatType.TEXT) {
+          let source;
+          if (type === 'json' || type === 'text') {
             source = xhr.responseText;
           }
-          else if (type == FormatType.XML) {
+          else if (type === 'xml') {
             source = xhr.responseXML;
             if (!source) {
               source = new DOMParser().parseFromString(xhr.responseText, 'application/xml');
             }
           }
-          else if (type == FormatType.ARRAY_BUFFER) {*/
-          source = (xhr.response);
-          // }
+          else if (type === 'arraybuffer') {
+            source = xhr.response;
+          }
           if (source) {
             success.call(this, format.readFeatures(source, {
               extent,
               featureProjection: projection
             }), format.readProjection(source));
           }
+          else {
+            // TODO
+            failure.call(this);
+          }
+        } else {
+          // TODO
+          failure.call(this);
         }
+      };
+      xhr.onerror = () => {
+        // TODO
+        failure.call(this);
       };
       xhr.send();
     });
   }
-
 }
